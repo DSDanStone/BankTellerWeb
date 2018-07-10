38da1917f36c940975df5d19720979a33d139266
Daniel Stone <ds.danstone@gmail.com>
2018-07-10 08:49:50 -0400

    Add project files.

---
+++ BankTellerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BankTellerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
